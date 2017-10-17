b13ec099f58f80f32cc11e8899f75814d044a16a
skaria <skvarghe@in.ibm.com>
2017-10-17 15:02:52 +0530

    150252

--- server.js
+++ server.js
@@ -156,11 +156,7 @@ app.post('/submittime', function (req, res){
       res.status(500).send(resp);
     } // End of if (err)
     var id=-1;
-    connection.query('INSERT INTO timesheets ("uid","weekending","d1","d2","d3","d4","other","task","hours") values ' + req.body.insert, function (err,rows) {
-      console.log("~~~~~~~~~~~~~INSERT INTO timesheets ('uid','weekending','d1','d2','d3','d4','other','task','hours') values " + req.body.insert);
-      //console.log('~~~~~~~~~~~~~'+JSON.stringify(req.body).insert);
-      //console.log('~~~~~~~~~~~~~'+JSON.parse(req.body).insert);
-      console.log('~~~~~~~~~~~~~'+req.body.insert);
+    connection.query('INSERT INTO timesheets (uid,weekending,d1,d2,d3,d4,other,task,hours) values ' + req.body.insert, function (err,rows) {
       if (!err) {
         connection.release();
         resp.appcode="100";
